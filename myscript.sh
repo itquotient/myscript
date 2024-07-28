98dac7c1fa299144a730a156ddb30b293cf9565c
Richard Igwegbu <itquotient8@gmail.com>
2024-07-28 16:22:53 -0500

    Created .gitignore file, etc.bak, myscript, and secret.keys

---
+++ myscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Welcome to UTA"
+echo ""
+echo "###################################################################"
+echo "Checking Available disk Space"
+df -hT
+echo ""
+echo "##################################################################"
+echo "Checking Available Memory"
+free -m
